7635d735a2e598560837a9e31cc44653b0a7e5b9
CREOTOPI <sajeer.ali09@gmail.com>
2026-05-25 14:27:59 +0400

    update

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@
 const sections = document.querySelectorAll(".scroll-section");
 
 const sectionObserver = new IntersectionObserver(
-    (entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("show");
-            }
-        });
-    },
-    {
-        threshold: 0.2,
-    }
+  (entries) => {
+    entries.forEach((entry) => {
+
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+
+    });
+  },
+  {
+    threshold: 0.2,
+  }
 );
 
 sections.forEach((section) => {
-    sectionObserver.observe(section);
+  sectionObserver.observe(section);
 });
 
 
